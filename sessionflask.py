37ae21aecfa08b64a17150cd712664f23d37dc44
Bisman <bismanbedi2006@gmail.com>
2025-07-26 02:36:39 +0530

    Doctors-web-app

--- sessionflask.py
+++ sessionflask.py
@@ -33,13 +33,18 @@ def login():
 # this funtion is for the home when doctor register or login
 @web_app.route('/home',methods=['GET'])
 def home():
-    return render_template ('home.html', name = session['name'], email = session['email'])
+    if len(session['user_id']) > 0:
+      return render_template ('home.html', name = session['name'], email = session['email'])
+    else:
+        return redirect('/')
 
 #this is to add patient
 @web_app.route('/add-patient',methods=['GET'])
 def add_patient():
-    return render_template ('add-patient.html', name = session['name'], email = session['email'])
-
+    if len(session['user_id']) > 0 :
+        return render_template ('add-patient.html', name = session['name'], email = session['email'])
+    else:
+        return redirect('/')
 #this is 
 @web_app.route('/adduser', methods=['POST','GET'])
 def add_user_in_db():
@@ -66,27 +71,32 @@ def add_user_in_db():
 #this is for add patients in database      
 @web_app.route('/add-patient-in-db', methods=['POST','GET'])
 def add_pateint_in_db():
-    print("testing")
-    patient = Patient()
-    patient.name = request.form['name']
-    patient.phone = request.form['phone']
-    patient.email = request.form['email']
-    patient.address = request.form['address']
-    patient.gender = request.form['gender']
-    patient.age = request.form['age']
-    patient.doctor_id = session['user_id'] # from session we have taken the doctor id
+     if len(session['user_id']) > 0:
+        print("testing")
+        patient = Patient()
+        patient.name = request.form['name']
+        patient.phone = request.form['phone']
+        patient.email = request.form['email']
+        patient.address = request.form['address']
+        patient.gender = request.form['gender']
+        patient.age = request.form['age']
+        patient.doctor_id = session['user_id'] # from session we have taken the doctor id
+        
+        print(patient.to_document())
+        db.select_db(collection='patients')
+        result = db.insert(document=patient.to_document())
+        
+        if len(str(result.inserted_id)) >0 :
+        
+            return render_template('/home.html', name = session['name'] , email = session['email'])
     
-    print(patient.to_document())
-    db.select_db(collection='patients')
-    result = db.insert(document=patient.to_document())
+        else:
+            return'somrthing went wrong'
     
-    if len(str(result.inserted_id)) >0 :
-       
-         return render_template('/home.html', name = session['name'] , email = session['email'])
-  
-    else:
-          return'somrthing went wrong'
-      
+     else:
+         return redirect('/')
+    
+        
       
 @web_app.route('/fetch-user', methods=['POST'])
 def fetch_user_from_db():
@@ -107,23 +117,36 @@ def fetch_user_from_db():
          return render_template('home.html', name = user['name'], email = user['email'])
     else:
         return 'Username or Password Invalid. Please Try Again'
+ 
+@web_app.route('/logout')
+def logout():
+     session['user_id'] = ''
+     session['name'] = ''
+     session['email'] = ''
+     
+     return redirect('/')
    
    
 @web_app.route('/fetch-patients')
 def fetch_patients_from_db():
-    query = {
-        'doctor_id': session['user_id']
-    }
-    db.select_db(collection='patients')
-    documents = db.fetch(query)
-
-    if len(documents) > 0:
-         return render_template('show-patients.html', name = session['name'], 
-                                email = session['email'], total = len(documents), patients = documents)
-    else:
-        return 'Patients not found'   
-   
     
+     if len(session['user_id']) > 0:
+        query = {
+            'doctor_id': session['user_id']
+        }
+        db.select_db(collection='patients')
+        documents = db.fetch(query)
+
+        if len(documents) > 0:
+            return render_template('show-patients.html', name = session['name'], 
+                                    email = session['email'], total = len(documents), patients = documents)
+        else:
+            return 'Patients not found'   
+    
+     else:
+        return redirect('/')
+    
+
 
 def main():
     # Secret Key, we have to create manually of our choice
